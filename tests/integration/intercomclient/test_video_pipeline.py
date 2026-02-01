c15eaca85a067470c5ce8773cd3591506ce7ed0e
Kieran Manning <mail@kmanning.ie>
2026-02-01 19:25:47 +0000

    chore: cleaning

--- tests/integration/intercomclient/test_video_pipeline.py
+++ tests/integration/intercomclient/test_video_pipeline.py
@@ -17,20 +17,6 @@ class TestVideoPipeline:
         capture = pipeline.init_camera(source=0)
         assert capture.isOpened()
 
-    # def test_init_writer(self, config):
-    #     with tempfile.TemporaryDirectory() as tmp_output_dir_path:
-    #         config.output_dir_path = tmp_output_dir_path
-    #         pipeline = VideoPipeline(config=config)
-    #         writer = pipeline.init_writer(0)
-    #         assert writer.isOpened()
-
-    # def test_capture_frame(self, config):
-    #     pipeline = VideoPipeline(config=config)
-    #     capture = pipeline.init_camera(source=0)
-    #     frame = pipeline.capture_frame()
-    #     capture.release()
-    #     assert frame is not None
-
     def test_record_segment(self, config):
         with tempfile.TemporaryDirectory() as tmp_output_dir_path:
             config.output_dir_path = tmp_output_dir_path
