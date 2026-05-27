8d1ffe49ad942403524ac25054e7fe20b0aab305
Kieran Manning <mail@kmanning.ie>
2026-05-27 20:35:15 +0100

    fix: remove old tests

--- tests/integration/conftest.py
+++
@@ -1,4 +0,0 @@
-from intercomclient.config import Config
-
-config = Config
-config.segment_duration = 2
--- tests/integration/intercomclient/test_aws.py
+++
@@ -1,7 +0,0 @@
-from intercomclient.aws import DynamoDBClient
-
-
-class TestDynamoDBClient:
-    def test_auth_check(self):
-        client = DynamoDBClient(boto3_client=None)
-        assert client.auth_check() is not None
